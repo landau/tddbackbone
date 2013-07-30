c7ce7482851eb4c58c5766e2bbb553c418b7568b
Trevor <landautrevor@gmail.com>
2013-07-30 18:10:59 -0400

    use mock instead for example

--- js/test/specs/todo.spec.js
+++ js/test/specs/todo.spec.js
@@ -14,25 +14,30 @@
             beforeEach(function () {
                 // Prevent sync from doing it's normal thing
                 // when `save` is called
-                this.syncStub = sinon.stub(app.Todo.prototype, "sync");
+                this.mock = sinon.mock(app.Todo.prototype);
+                this.expect = this.mock.expects('sync');
                 this.model = new app.Todo();
             });
 
             afterEach(function () {
-                app.Todo.prototype.sync.restore();
+                this.mock.restore();
             });
 
-            it('should call save', function () {
+            it('should call sync', function () {
+                this.expect.once();
                 this.model.toggle();
-                expect(this.syncStub).to.have.been.calledOnce;
+                this.mock.verify();
             });
 
             it('should set completed to it\'s opposite state', function () {
+                this.expect.twice();
+
                 this.model.toggle();
                 expect(this.model.get('completed')).to.be.true;
 
                 this.model.toggle();
                 expect(this.model.get('completed')).to.be.false;
+                this.mock.verify();
             });
         });
     });
